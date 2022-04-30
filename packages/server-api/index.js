5004c8f2e043d69627086ff3335dea0a2a75bbe6
OmGole <omgole82@gmail.com>
2022-04-30 11:53:09 +0530

    removed sub

--- packages/server-api/index.js
+++ packages/server-api/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
-const {add,sub} = require("./node_modules/add/index")
+const {add} = require("./node_modules/add/index")
 
 
 console.log(add(2,2));
-console.log(sub(2,2));
+
 
 app.listen(port,(err) => {
   if(err) {
